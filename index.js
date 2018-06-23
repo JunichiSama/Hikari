73175baf57facbb246854d55580fc3e002b01c45
JunichiSama <37641336+JunichiSama@users.noreply.github.com>
2018-06-23 14:56:20 -0300

    Sistema de Ban Añadido

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ hikari.on("message", async message =>{
 
   let kickEmbed = new Discord.RichEmbed()
   .setDescription("~Kick~")
-  .setColor("#f44242")
+  .setColor("#42ebf4")
   .addField("Usuario Expulsado", `${kUser} with ID ${kUser.id} `)
   .addField("Expulsado Por", `<@${message.author.id}> with ID ${message.author.id} `)
   .addField("Expulsado En", message.channel)
@@ -42,6 +42,35 @@ hikari.on("message", async message =>{
 
  message.guild.member(kUser).kick(kReason);
   kickChannel.send(kickEmbed);
+    return;
+  }
+
+  if(cmd === `${prefix}ban` ){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("No Puedo Encontrar al Usuario.");
+    let bReason = args.join("  ").slice(22);
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("No Puedes usar el Comando!");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("Esa Persona no Puede ser Expulsada");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Baneado~")
+    .setColor("#41f450")
+    .addField("Usuario Baneado", `${bUser} with ID ${bUser.id} `)
+    .addField("Baneado Por", `<@${message.author.id}> with ID ${message.author.id} `)
+    .addField("Baneado En", message.channel)
+    .addField("Hora", message.createdAt)
+    .addField("Razon", bReason);
+
+    let banChannel = message.guild.channels.find("name", "incidentes");
+    if(!banChannel) return message.channel.send("No Encuentro el Canal de Incidentes!");
+
+
+   message.guild.member(bUser).ban(bReason);
+    banChannel.send(banEmbed);
+
+
+
     return;
   }
 
